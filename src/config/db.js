a847cfafb088cd02b9102ad77391c86efd7e7baf
dkempfen <107085437+dkempfen@users.noreply.github.com>
2025-04-02 20:49:06 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,19 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const connectDB = async () => {
-  try {
-    console.log("Intentando conectar a MongoDB..."); 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB conectado");
-  } catch (error) {
-    console.error("Error conectando a MongoDB:", error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB conectado');
+    } catch (error) {
+        console.error('Error conectando a MongoDB:', error);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
